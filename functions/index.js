034224db59dcc4a55ad1f7156eef4e82cacd1a06
Yuki Okada <y.okady.221@gmail.com>
2020-07-05 11:16:40 +0900

    Jリーグとプレミアリーグの一部を追加

--- functions/index.js
+++ functions/index.js
@@ -6,34 +6,70 @@ const moment = require('moment');
 firebaseAdmin.initializeApp();
 
 const countries = {
-  'SPAIN': {
+  'JAPAN': [{
+    channelId: 'UCWc-XpFHPK1SwGcvpFPZ8NA',
+    filter: (title) => title.startsWith('【公式】ハイライト：'),
+  }],
+  'SPAIN': [{
     channelId: 'UCTv-XvfzLX3i4IGWAm4sbmA',
-    filter: (video) =>
-      video.snippet.title.startsWith('Highlights '),
-  },
-  'ITALY': {
+    filter: (title) => title.startsWith('Highlights '),
+  }],
+  'ITALY': [{
     channelId: 'UCBJeMCIeLQos7wacox4hmLQ',
-    filter: (video) =>
-      !video.snippet.title.includes(' Post Match ') &&
-      !video.snippet.title.includes(' Top Moment ') &&
-      / [0-9]+-[0-9]+ /.test(video.snippet.title)
-  },
+    filter: (title) =>
+      !title.includes(' Post Match ') && !title.includes(' Top Moment ') && / [0-9]+-[0-9]+ /.test(title)
+  }],
+  'ENGLAND': [
+    {
+      // Chelsea
+      channelId: 'UCU2PacFf99vhb3hNiYDmxww',
+      filter: (title) => title.includes('Highlights') && / [0-9]+-[0-9]+ /.test(title),
+    },
+    {
+      // Arsenal
+      channelId: 'UCpryVRk_VDudG8SHXgWcG0w',
+      filter: (title) => title.includes('Highlights') && / [0-9]+-[0-9]+ /.test(title),
+    },
+    {
+      // Manchester United
+      channelId: 'UC6yW44UGJJBvYTlfC7CRg2Q',
+      filter: (title) => title.includes('Highlights') && / [0-9]+-[0-9]+ /.test(title),
+    },
+    {
+      // Liverpool
+      channelId: 'UC9LQwHZoucFT94I2h6JOcjw',
+      filter: (title) => title.includes('Highlights') && / [0-9]+-[0-9]+ /.test(title),
+    },
+    {
+      // Man City
+      channelId: 'UCkzCjdRMrW2vXLx8mvPVLdQ',
+      filter: (title) => title.includes('HIGHLIGHTS') && / [0-9]+-[0-9]+ /.test(title),
+    },
+    {
+      // Tottenham
+      channelId: 'UCEg25rdRZXg32iwai6N6l0w',
+      filter: (title) => title.includes('HIGHLIGHTS') && / [0-9]+-[0-9]+ /.test(title),
+    },
+  ],
 };
 
-exports.syncVideos = functions.pubsub.schedule('every 60 minutes').onRun(async (context) => {
+exports.syncVideos = functions.pubsub.schedule('every 60 minutes').onRun((context) => {
   const publishedAfter = moment().subtract(90, 'minutes').toISOString();
-  await Promise.all(Object.keys(countries).map((key) => syncVideosByCountry(key, publishedAfter)));
+  return Promise.all(Object.keys(countries).map((key) => syncVideosByCountry(key, publishedAfter)));
 });
 
-const syncVideosByCountry = async (key, publishedAfter) => {
-  const videos = await getVideos(countries[key].channelId, publishedAfter);
-  await Promise.all(videos.filter(countries[key].filter).map((video) =>
-    firebaseAdmin.firestore().collection('videos').doc(video.id.videoId).set({
-      ...video,
-      country: key,
-      publishedAt: firebaseAdmin.firestore.Timestamp.fromDate(new Date(video.snippet.publishedAt)),
-    }, { merge: true })));
-}
+const syncVideosByCountry = (key, publishedAfter) =>
+  Promise.all(countries[key].map((team) =>
+    getVideos(team.channelId, publishedAfter).then((videos) =>
+      Promise.all(videos.filter((video) => team.filter(video.snippet.title)).map((video) =>
+        firebaseAdmin.firestore().collection('videos').doc(video.id.videoId).set({
+          ...video,
+          country: key,
+          publishedAt: firebaseAdmin.firestore.Timestamp.fromDate(new Date(video.snippet.publishedAt)),
+        }, { merge: true })
+      ))
+    )
+  ));
 
 const getVideos = async (channelId, publishedAfter) => {
   const youtube = google.youtube({
